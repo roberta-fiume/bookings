011a6e6ce79a6a11eb4ec5583feb68af3c465f77
roberta-fiume <robertaf898@gmail.com>
2021-01-08 11:30:51 +0000

    added calendar with vuetify

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,21 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Calendar/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import Calendar from './components/Calendar';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Calendar
   },
 
   data: () => ({
